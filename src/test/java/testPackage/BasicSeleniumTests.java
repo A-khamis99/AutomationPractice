b1e9ed6503ab889579c27f5e07a13d3fdc679688
Abdelrahman_Khamis <92450036+A-khamis99@users.noreply.github.com>
2026-06-27 13:37:57 +0300

    Add setup and teardown methods for Selenium tests

--- src/test/java/testPackage/BasicSeleniumTests.java
+++ src/test/java/testPackage/BasicSeleniumTests.java
@@ -2,13 +2,26 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicSeleniumTests {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+       driver  = new EdgeDriver();
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Test
     public void test() {
-        WebDriver driver = new EdgeDriver();
         driver.get("https://www.example.com");
-
+        System.out.println("Page title is: " + driver.getTitle());
     }
+
 }
